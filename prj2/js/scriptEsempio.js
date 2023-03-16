d7b7f01e992d33e23172fde3f509cb62e39c3e31
Dario <104058513+DarioIEL@users.noreply.github.com>
2023-03-16 17:44:27 +0100

    agg

---
+++ prj2/js/scriptEsempio.js
@@ -0,0 +1,8 @@
+// voglio creare un elenco di film con relativa durata e mostrarli nella pagina nel div #elenco
+//stampa il totale dei minuti nel div #sommaMinuti
+//stampa un saluto all'utente nel p #benvenuto
+
+
+var film = ["Venom", "Dune", "John Wick", "Fight Club", "Interstellar"];
+var durata = [100, 140, 90, 120, 160];
+
