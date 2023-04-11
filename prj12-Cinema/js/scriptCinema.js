39b31ce7da759f63788ee19680efd346696ac5db
FacuNor <38099268+FacuNor@users.noreply.github.com>
2023-04-11 14:05:46 +0200

    Esercizio Finito

--- prj12-Cinema/js/scriptCinema.js
+++ prj12-Cinema/js/scriptCinema.js
@@ -23,5 +23,112 @@ var filmsJSON = [
         durata: 170,
         poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg" ,
         trama: "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence."
+    },
+    {
+        titolo: "Blade Runner",
+        regista: "Ridley Scott",
+        attori:["Harrison Ford",
+        "Rutger Hauer", 
+        "Sean Young"
+        ],
+        durata: 117,
+        poster: "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+        trama:"A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
+    },
+    
+    {
+        titolo: "Drive",
+        regista: "Nicolas Winding Refn",
+        attori:["Ryan Gosling",
+        "Carey Mulligan", 
+        "Bryan Cranston"
+        ],
+        durata: 100,
+        poster: "https://m.media-amazon.com/images/M/MV5BZjY5ZjQyMjMtMmEwOC00Nzc2LTllYTItMmU2MzJjNTg1NjY0XkEyXkFqcGdeQXVyNjQ1MTMzMDQ@._V1_SX300.jpg",
+        trama:"A mysterious Hollywood action film stuntman gets in trouble with gangsters when he tries to help his neighbor's husband rob a pawn shop while serving as his getaway driver.",
+    },
+
+    {
+        titolo: "Interstellar",
+        regista: "Christofer Nolan",
+        attori:["Matthew McConaughey",
+        " Anne Hathaway", 
+        "Jessica Chastain"
+        ],
+        durata: 169,
+        poster: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+        trama:"A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
     }
 ]
+var titolo = document.querySelector("#titolo");
+var locandina = document.querySelector("#locandina");
+var regista = document.querySelector("#regista");
+var attori = document.querySelector("#attori");
+var durata = document.querySelector("#durata");
+var btnTrama = document.querySelector("#btnTrama");
+var trama = document.querySelector("#trama");
+var btnIndietro = document.querySelector("#btnIndietro");
+var btnAvanti = document.querySelector("#btnAvanti");
+
+var indice = 0;
+btnAvanti.addEventListener("click",avanti);
+btnIndietro.addEventListener("click",indietro);
+btnTrama.addEventListener("click",visTrama);
+
+function avanti(){
+    indice++;
+    if(indice == filmsJSON.length){
+        indice = 0;
+    }
+    visualizza();
+    flag = false;
+    visTrama();
+}
+
+function indietro(){
+    indice--;
+    if(indice<0){
+        indice = filmsJSON.length - 1;
+    }
+    visualizza();
+    flag = false;
+    visTrama();
+}
+
+function visualizza() {
+    titolo.textContent = filmsJSON[indice].titolo;
+    
+    locandina.setAttribute("src",filmsJSON[indice].poster);
+
+    regista.textContent = filmsJSON[indice].regista;
+
+    //Attori
+    while(attori.firstChild){
+        attori.removeChild(attori.firstChild);
+    }
+    filmsJSON[indice].attori.forEach(attore =>{
+        let riga = document.createElement("li");
+        let wiki = document.createElement("a");
+        wiki.setAttribute("href","https://it.wikipedia.org/wiki/"+attore);
+        wiki.textContent = attore;
+
+        attori.appendChild(riga);
+        riga.appendChild(wiki);
+    });
+
+    durata.textContent = filmsJSON[indice].durata + " minuti";
+
+}
+
+var flag = true;
+function visTrama(){
+    if(flag) {
+        trama.textContent = filmsJSON[indice].trama;
+        flag = false;
+    }else{
+        trama.textContent = "";
+        flag = true
+    }
+    
+}
+visualizza();
